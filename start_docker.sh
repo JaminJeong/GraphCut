c159e05e1fcb7c895b1f227605be84bb95f8c344
yongmin.park <yongmin.park@continuumai.co.kr>
2018-12-27 14:42:41 +0900

    modified start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
-docker run -it --net=host --privileged -e DISPLAY --name 'graphcut' \
+docker run -it --net=host --privileged -e DISPLAY --name 'graphcut_update' \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /etc/group:/etc/group:ro \
   -v /etc/passwd:/etc/passwd:ro \
@@ -7,5 +7,6 @@ docker run -it --net=host --privileged -e DISPLAY --name 'graphcut' \
   -u=$UID:$(id -g $USER) \
   -p 9999:8888 \
   -p 7007:6006 \
+  --env QT_X11_NO_MITSHM=1 \
   mooneclipse03/graphcut \
   /bin/bash
